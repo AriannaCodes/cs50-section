7d38090dd5164f0b32b4747f185c7d48007e64e7
Arianna Benson <ariannabenson@gmail.com>
2015-09-23 16:33:50 +0000

    added problem 1

--- 1/problem-1.c
+++ 1/problem-1.c
@@ -5,6 +5,11 @@
 
 int main(void)
 {
-    string s = GetString();
-    System.out.println(s);
+    int ages[] = {3, 12, 14, 5, 55};
+    int sum = 0;
+    // iterate over ages, adding them to sum
+    for (int i = 0; i < 5; i++) {
+        sum += ages[i];
+    }
+    printf("The sum of the ages is %i\n", sum);
 }
