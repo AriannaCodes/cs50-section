fee83f5402bbc490613624af36924444f7ba85d7
Arianna Benson <ariannabenson@gmail.com>
2015-09-28 23:08:37 +0000

    added problem 1

--- 2/problem-1.c
+++ 2/problem-1.c
@@ -3,7 +3,31 @@
 #include <stdio.h>
 #include <string.h>
 
+bool search(int value, int values[], int n)
+{
+    // Set values for the top and the bottom of the search
+    int lower = 0;
+    int upper = n - 1;
+
+    // Binary search
+    while (lower <= upper)
+    {
+        // insert code here
+    }
+
+    return false;
+}
+
+
 int main(void)
 {
-    return;
+    int values[] = {3, 2, 10, 1, 5};
+    if (search(1, values, 5))
+    {
+        printf("Success!\n");
+    }
+    else
+    {
+        printf("Failure.\n");
+    }
 }
